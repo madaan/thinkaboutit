aecdcbd9e2af1d5fd30afe5a504e3e1864750ec4
Aman Madaan <amadaan@cs.cmu.edu>
2021-10-09 22:58:48 -0400

    Add inference scripts for MOE

---
+++ scripts/infer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+GPU="${1}"
+MODEL_TYPE=${2}
+CKPT="${3}"
+OUTPUT_LOC="${4}"
+GRAPH_FILE_NAME="${5}"
+export CUDA_VISIBLE_DEVICES=${GPU} && python src/gnn_qa/model/${MODEL_TYPE}/infer.py --ckpt ${CKPT} \
+    --paths_output_loc ${OUTPUT_LOC}\
+     --graphs_file_name ${GRAPH_FILE_NAME}
